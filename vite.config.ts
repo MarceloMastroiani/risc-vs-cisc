69d4d7ae044f1b0d19251f2ada818da772cbaab5
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2026-06-24 17:01:34 -0300

    fix: nuevo nombre del proyecto, cambios para github page

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: "/arquitectura-coloquio/",
+  base: "/risc-vs-cisc/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
